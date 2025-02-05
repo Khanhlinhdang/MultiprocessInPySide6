960e23b90c989bb15d19f09f65dbc496dbb3f760
Dang Khanh Linh <khanhlinhdangthditrach@gmail.com>
2025-02-05 11:12:10 +0300

    add example multiprocess and multilthead and thead with async

---
+++ main.py
@@ -0,0 +1,67 @@
+import sys
+from concurrent.futures import Future
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
+import pandas as pd
+
+from return_worker import ReturnProcess, ProcessPool_Executor,ThreadPool_Executor
+# Hàm xử lý nặng (chạy trong process pool)
+
+class MainWindow(QMainWindow):
+    n = 0
+    def __init__(self):
+        super().__init__()
+        ProcessPool_Executor.submit(print,"start process")
+        ProcessPool_Executor.submit(print,"start threads")
+        self.setup_ui()
+        
+    def setup_ui(self):
+        self.setWindowTitle("ProcessPoolExecutor Example")
+        self.setGeometry(100, 100, 400, 200)
+        self.label = QLabel("Click Start to run task", self)
+        self.btn_start = QPushButton("Start Task", self)
+        self.btn_start.clicked.connect(self.start_task)
+        
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.btn_start)
+        
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+        self.n = 0
+        
+    @classmethod
+    def heavy_task(cls):
+        df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+        cls.n +=1
+        # print("check ",cls.n)
+        data = df.to_dict()
+        df = pd.DataFrame.from_dict(data=data)
+        return df 
+    
+    def start_task(self):
+        print("self ",self.n)
+        self.label.setText("Processing...")
+        self.executor = ReturnProcess(self.heavy_task)
+        self.executor.update_signal.connect(self.update_label)
+        self.executor.finished_signal.connect(self.task_finished)
+        self.executor.start()
+        
+    def update_label(self, future: Future):
+        print(future,type(future))
+        self.label.setText(str(future))
+
+    def task_finished(self):
+        self.btn_start.setEnabled(True)
+        self.label.setText("Task completed!")
+        self.future.result()  # Xử lý exception nếu có
+
+    def closeEvent(self, event):
+        self.executor.close()
+        event.accept()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ return_worker.py
@@ -0,0 +1,102 @@
+import asyncio
+from concurrent.futures import Future
+from typing import Optional
+from psygnal import Signal
+from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
+
+global ProcessPool_Executor,ThreadPool_Executor
+
+ProcessPool_Executor,ThreadPool_Executor = ProcessPoolExecutor(max_workers=4,max_tasks_per_child=10), ThreadPoolExecutor(max_workers=4)
+
+class ReturnProcess():
+    update_signal = Signal(object)
+    finished_signal = Signal()
+    def __init__(self,fn:callable=None,callback:Optional[callable]=None, *args, **kwargs):
+        self.fn = fn
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs.copy()
+        self.executor = ProcessPool_Executor
+
+    def start(self):
+        self.future = self.executor.submit(
+            self.fn,
+            *self.args, 
+            **self.kwargs
+        )
+        if self.callback:
+            self.future.add_done_callback(self.callback)
+        else:
+            self.future.add_done_callback(self._callback)
+            
+    def _callback(self, future: Future):
+        # print(future.result())
+        self.update_signal.emit(future.result())
+        return future.result()
+
+    def close(self):
+        self.executor.shutdown(wait=False)
+
+class AsyncThread():
+    update_signal = Signal(object)
+    finished_signal = Signal()
+    def __init__(self,fn:callable=None,callback:Optional[callable]=None, *args, **kwargs):
+        self.fn = fn
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs.copy()
+        self.executor = ThreadPool_Executor
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+    
+    def start(self):
+        self.future = self.executor.submit(self.run)
+        if self.callback:
+            self.future.add_done_callback(self.callback)
+        else:
+            self.future.add_done_callback(self._callback)
+    
+    def _callback(self, future: Future):
+        # print(future.result())
+        self.update_signal.emit(future.result())
+        return future.result()
+        
+    def run(self):
+        try:
+            self.loop.run_until_complete(self.fn(*self.args, **self.kwargs))
+            # asyncio.run(self.fn(*self.args, **self.kwargs))
+        except Exception as e:
+            self.loop.close()
+        finally:
+            self.deleteLater()
+    def close(self):
+        self.executor.shutdown(wait=False)
+
+class SyncThread():
+    update_signal = Signal(object)
+    finished_signal = Signal()
+    def __init__(self,fn:callable=None,callback:Optional[callable]=None, *args, **kwargs):
+        self.fn = fn
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs.copy()
+        self.executor = ThreadPool_Executor
+
+    def start(self):
+        self.future = self.executor.submit(
+            self.fn,
+            *self.args, 
+            **self.kwargs
+        )
+        if self.callback:
+            self.future.add_done_callback(self.callback)
+        else:
+            self.future.add_done_callback(self._callback)
+            
+    def _callback(self, future: Future):
+        # print(future.result())
+        self.update_signal.emit(future.result())
+        return future.result()
+
+    def close(self):
+        self.executor.shutdown(wait=False)
